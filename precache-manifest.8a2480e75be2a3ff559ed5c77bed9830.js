1847a4c53b907037124308bc7e0413300b11db65
kurt <Kurt.Dhaene@UGent.be>
2019-03-25 13:23:59 +0100

    Updates

---
+++ precache-manifest.8a2480e75be2a3ff559ed5c77bed9830.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "ee7cd8ed2dcec943251eb2763684fc6f",
+    "url": "/trafficlightsdemo/static/media/logo.ee7cd8ed.svg"
+  },
+  {
+    "revision": "2f8f9be54b4f19fc067c",
+    "url": "/trafficlightsdemo/static/js/runtime~main.2f8f9be5.js"
+  },
+  {
+    "revision": "41dacd80bcf7b755e01d",
+    "url": "/trafficlightsdemo/static/js/main.41dacd80.chunk.js"
+  },
+  {
+    "revision": "856103040339a4e2d3c4",
+    "url": "/trafficlightsdemo/static/js/2.85610304.chunk.js"
+  },
+  {
+    "revision": "41dacd80bcf7b755e01d",
+    "url": "/trafficlightsdemo/static/css/main.e5d960e6.chunk.css"
+  },
+  {
+    "revision": "8f3e6cecfe0b74419bd2364150490cc8",
+    "url": "/trafficlightsdemo/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/trafficlightsdemo/precache-manifest.92cfa50a1363606603d2e01c2f19deba.js"
+  "/trafficlightsdemo/precache-manifest.8a2480e75be2a3ff559ed5c77bed9830.js"
 );
 
 workbox.clientsClaim();
