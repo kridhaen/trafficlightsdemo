f9a6a1f2b03598b4c80900f439d1e94facf5ff3b
kurt <Kurt.Dhaene@UGent.be>
2019-03-25 13:49:30 +0100

    :text

--- src/App.js
+++ src/App.js
@@ -8,7 +8,8 @@ class App extends Component {
     return (
       <div className="App">
         <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
+          {/* <img src={logo} className="App-logo" alt="logo" /> */}
+          <p>This visualisation gives an idea of the remaining time in a certain phase for each trajectory. The colours represent the colours of the traffic lights in their current state. The left column shows the start positions, the following rows are the end positions.</p>
           <Drawer/>
         </header>
       </div>
