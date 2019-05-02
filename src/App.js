639dc09240078084ee2241d19e4ee79f4e14421b
Kurt Normaal <Kurt.Dhaene@UGent.be>
2019-05-02 20:56:55 +0200

    show drawer

--- src/App.js
+++ src/App.js
@@ -10,8 +10,8 @@ class App extends Component {
       <div className="App">
         <header className="App-header">
           {/*<img src={logo} className="App-logo" alt="logo" />*/}
-          {/*<Drawer/>*/}
-          <Prediction/>
+          <Drawer/>
+          {/*<Prediction/>*/}
         </header>
       </div>
     );
